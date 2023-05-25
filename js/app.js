16285d88214f53669ad3af1c8014bd500dbd25e9
JuanjoseGamboa <juangamboaortiz@gmail.com>
2023-05-25 15:52:11 -0500

    feat: :sparkles: Ejercicio2
    Se crea la clase estudiante la cual hereda de Persona y se crea el metodo estudiar

--- js/app.js
+++ js/app.js
@@ -6,24 +6,37 @@ class Persona{
         this.nombre=nombre;
         this.edad=edad;
         this.sexo=sexo;
-    }
+    };
 
     saludar(){
         return `¿Hola como estás?, mi nombre es ${this.nombre}`;
+    };
+};
+
+class Estudiante extends Persona{
+    constructor(nombre,edad,sexo,carrera){
+        super(nombre,edad,sexo);
+        this.carrera=carrera;
     }
-}
+
+    estudiar(){
+        return `Estoy estudiando la carrera de ${this.carrera}`;
+    }
+};
 
 //Seleccionar elementos
 
 const formulario1=document.querySelector('#formulario1');
 const saludo=document.querySelector('.saludo');
+const carrera=document.querySelector('.carrera');
 
 
 formulario1.addEventListener('submit',e=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
-    let persona1=new Persona(data.nombre,data.edad,data.sexo);
-    saludo.textContent=persona1.saludar();
+    let estudiante1=new Estudiante(data.nombre,data.edad,data.sexo,data.carrera);
+    saludo.textContent=estudiante1.saludar();
+    carrera.textContent= estudiante1.estudiar();
 })
 
 

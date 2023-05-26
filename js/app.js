baef85f8b653f73df965e1f174e8cd184824d6d2
Juan Jose Gamboa <juangamboaortiz@gmail.com>
2023-05-26 18:39:19 -0500

    feat: :sparkles: Ejerciicio9 - Clase Coche
    Se crea una clase coche que hereda de la clase vehiculo

--- js/app.js
+++ js/app.js
@@ -11,6 +11,18 @@ class Vehiculo{
 };
 
 
+class Coche extends Vehiculo{
+    constructor(marca,modelo,velocidad,combustible){
+        super(marca,modelo,velocidad,combustible)
+        this.combustible=combustible;
+    }
+
+    acelerar(){
+        return this.velocidad+=20;
+    };
+}
+
+
 
 //Seleccionar elementos
 
@@ -22,8 +34,8 @@ const area=document.querySelector('.area');
 formulario1.addEventListener('submit',e=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
-    let vehiculo1=new Vehiculo(data.marca,data.modelo,data.velocidad);
-    area.textContent=`La velocidad del vehiculo es ${vehiculo1.acelerar()} km/h`;
+    let coche1=new Coche(data.marca,data.modelo,data.velocidad,data.combustible);
+    area.textContent=`La velocidad del Coche es ${coche1.acelerar()} km/h`;
 })
 
 

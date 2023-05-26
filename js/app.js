36b21023516f199068421127b8d6dfdccc0dbc61
Juan Jose Gamboa <juangamboaortiz@gmail.com>
2023-05-26 18:50:33 -0500

    feat: :sparkles: Ejercicio11 - Clase empleado
    Se crea la clase empleado con el metodo de calcularSalarioAnual

--- js/app.js
+++ js/app.js
@@ -1,47 +1,32 @@
-class Vehiculo{
-    constructor(marca,modelo,velocidad){
-        this.marca=marca;
-        this.modelo=modelo;
-        this.velocidad=parseInt(velocidad);
+class Empleado{
+    constructor(nombre,edad,sueldo){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.sueldo=parseInt(sueldo);
     };
 
-    acelerar(){
-        return this.velocidad+=10;
+    calcularSalarioAnual(){
+        return this.sueldo*12;
     };
-
-    static convertirKmEnMph(velocidadkm){
-        return velocidadkm/1.6093;
-    }
 };
 
 
-class Coche extends Vehiculo{
-    constructor(marca,modelo,velocidad,combustible){
-        super(marca,modelo,velocidad,combustible)
-        this.combustible=combustible;
-    }
 
-    acelerar(){
-        return this.velocidad+=20;
-    };
-}
 
 
 
 //Seleccionar elementos
 
 const formulario1=document.querySelector('#formulario1');
-const velocidad1=document.querySelector('.velocidad1');
-const velocidad2=document.querySelector('.velocidad2');
+const salario=document.querySelector('.salario');
 
 
 
 formulario1.addEventListener('submit',e=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
-    let coche1=new Coche(data.marca,data.modelo,data.velocidad,data.combustible);
-    velocidad1.textContent=`La velocidad del Coche es ${coche1.acelerar()} km/h`;
-    velocidad2.textContent=`La velocidad del Coche es ${Vehiculo.convertirKmEnMph(coche1.velocidad)} en Mph`;
+    let empleado1=new Empleado(data.nombre,data.edad,data.sueldo);
+    salario.textContent= `El salario anual es  ${empleado1.calcularSalarioAnual()}$`;
 })
 
 

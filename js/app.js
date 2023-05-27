4bb218760a77b27799ac7fd126a124ec4f436053
Juan Jose Gamboa <juangamboaortiz@gmail.com>
2023-05-26 19:10:17 -0500

    feat: :sparkles: Ejercicio12-Clase EMpleado con variable estatica

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 class Empleado{
+    static variableEstatica=0;
     constructor(nombre,edad,sueldo){
+        this.id=Empleado.generarIdEmpleado();
         this.nombre=nombre;
         this.edad=edad;
         this.sueldo=parseInt(sueldo);
@@ -8,7 +10,24 @@ class Empleado{
     calcularSalarioAnual(){
         return this.sueldo*12;
     };
+
+    static generarIdEmpleado(){
+        Empleado.variableEstatica+=1
+        return Empleado.variableEstatica;
+    }
 };
+
+class Gerente extends Empleado{ 
+    constructor(nombre,edad,sueldo,departamento){
+        super(nombre,edad,sueldo);
+        this.departamento=departamento;
+    }
+
+    calcularSalarioAnual(){
+        return (this.sueldo*1.1)*12;
+
+    }
+}
 
 
 
@@ -18,15 +37,15 @@ class Empleado{
 //Seleccionar elementos
 
 const formulario1=document.querySelector('#formulario1');
-const salario=document.querySelector('.salario');
+const id=document.querySelector('.id');
 
 
 
 formulario1.addEventListener('submit',e=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
-    let empleado1=new Empleado(data.nombre,data.edad,data.sueldo);
-    salario.textContent= `El salario anual es  ${empleado1.calcularSalarioAnual()}$`;
+    let gerente1=new Gerente(data.nombre,data.edad,data.sueldo,data.departamento);
+    id.textContent= `El id del Gerente o empleado  es ${Empleado.generarIdEmpleado(gerente1)} `;
 })
 
 
